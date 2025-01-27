2c0f6d56221eb3e880a271db03aadeb1fe2f15e3
Miras <miras.ismail.2003@mail.ru>
2025-01-27 10:52:13 +0600

    emtixan7

---
+++ indexs.js
@@ -0,0 +1,17 @@
+let silteme = "https:api.jsonbin.io/v3/b/67965b2ee41b4d34e47efbb7"
+
+let btn=document.getElementById('btn') 
+
+async function fetchData(click="btn") {
+   
+   try{
+  
+   }catch{
+
+   }
+}
+fetchData()
+btn.addEventListener("click",function () {
+   alert("Деректер алынды")
+    
+})
--- Аралык бакылау 6/script.js
+++ Аралык бакылау 6/script.js
@@ -1,58 +1,70 @@
-let shop = {
-    name: "magnum",
-    phoneNamber: 87470437180,
-    products: [
-        {
-            productName: "samsung A-72",
-            category: "Элуктроника",
-            price: 220, 
-            stock:10
-        },
-        {
-            productName: "фудболка",
-            category: "киым",
-            price: 1500,
-            stock: 5
-        },
-        {
-            productName: "milk",
-            category:"прадукта",
-            price: 500,
-            stock:  30
-        },
-        {
-            productName: "kola",
-            category:"прадукта",
-            price: 300,
-            stock:  25
-        },
-        {
-            productName: "computer",
-            category:"электроника",
-            price: 500,
-            stock:  2
-        },
+// let shop = {
+//     name: "magnum",
+//     phoneNamber: 87470437180,
+//     products: [
+//         {
+//             productName: "samsung A-72",
+//             category: "Элуктроника",
+//             price: 220, 
+//             stock:10
+//         },
+//         {
+//             productName: "фудболка",
+//             category: "киым",
+//             price: 1500,
+//             stock: 5
+//         },
+//         {
+//             productName: "milk",
+//             category:"прадукта",
+//             price: 500,
+//             stock:  30
+//         },
+//         {
+//             productName: "kola",
+//             category:"прадукта",
+//             price: 300,
+//             stock:  25
+//         },
+//         {
+//             productName: "computer",
+//             category:"электроника",
+//             price: 500,
+//             stock:  2
+//         },
         
-    ],
+//     ],
 
-}
+// }
 
-console.log(shop);
+// console.log(shop);
 
-let nameMagazin  = prompt("дукен атауын озгертыныз") 
-changeShopName(nameMagazin)
-function changeShopName(magazinAty){
-    shop.name=magazinAty
-}
-console.log(shop)
-let nameNember  = prompt("номер озгерту")
-changePhoneNumber(nameNember)
- function changePhoneNumber(newNember) {
-    shop.phoneNamber=nameNember
- }  
- console.log(shop);
+// let nameMagazin  = prompt("дукен атауын озгертыныз") 
+// changeShopName(nameMagazin)
+// function changeShopName(magazinAty){
+//     shop.name=magazinAty
+// }
+// console.log(shop)
+// let nameNember  = prompt("номер озгерту")
+// changePhoneNumber(nameNember)
+//  function changePhoneNumber(newNember) {
+//    if (nameNember=="") {
+    
+//    }else{
+//     shop.phoneNamber=nameNember
+//    }
+//  }  
+//  shop.changeShopName()
+//  console.log(shop);
 //  function listProductNames(params) {
-//    return `${shop[0].productName}\n${shop[1].productName}\n${shop[2].productName}\n${shop[3].productName}`
+//   let produclist =[
+//     shop.products.forEach(function (params) {
+//         produclist.push(params.)
+//     })
+//   ]
 //  }
 //  alert(listProductNames())
  
+
+
+
